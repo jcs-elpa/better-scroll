f87c21c7b29ba1545f7e1f050325dd85d478de76
JenChieh <jcs090218@gmail.com>
2026-01-01 13:57:42 +0800

    chore: Update copyright year

--- better-scroll.el
+++ better-scroll.el
@@ -1,6 +1,6 @@
 ;;; better-scroll.el --- Improve user experience when scrolling window  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2020-2025  Shen, Jen-Chieh
+;; Copyright (C) 2020-2026  Shen, Jen-Chieh
 ;; Created date 2020-07-23 18:26:34
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
